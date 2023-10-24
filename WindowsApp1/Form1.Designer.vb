e61a65f4cecd02aa7762b1e77c2896a073b5506e
vf2_c <vf2_c@DESKTOP-U8NIMCR>
2023-10-24 18:11:29 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,299 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Task = New System.Windows.Forms.Timer(Me.components)
+        Me.Closeing = New System.Windows.Forms.Button()
+        Me.maxbox = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Version = New System.Windows.Forms.Label()
+        Me.nW = New System.Windows.Forms.Label()
+        Me.nH = New System.Windows.Forms.Label()
+        Me.nL = New System.Windows.Forms.Label()
+        Me.nT = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.DimGray
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 40)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(1280, 720)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'Task
+        '
+        Me.Task.Interval = 8
+        '
+        'Closeing
+        '
+        Me.Closeing.FlatAppearance.BorderSize = 0
+        Me.Closeing.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Closeing.Location = New System.Drawing.Point(1248, 8)
+        Me.Closeing.Name = "Closeing"
+        Me.Closeing.Size = New System.Drawing.Size(26, 23)
+        Me.Closeing.TabIndex = 38
+        Me.Closeing.Text = "×"
+        Me.Closeing.UseVisualStyleBackColor = True
+        '
+        'maxbox
+        '
+        Me.maxbox.FlatAppearance.BorderSize = 0
+        Me.maxbox.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.maxbox.Location = New System.Drawing.Point(1216, 8)
+        Me.maxbox.Name = "maxbox"
+        Me.maxbox.Size = New System.Drawing.Size(26, 23)
+        Me.maxbox.TabIndex = 39
+        Me.maxbox.Text = "□"
+        Me.maxbox.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(45, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(120, 17)
+        Me.Label1.TabIndex = 40
+        Me.Label1.Text = "Capture-Window "
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Meiryo UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.DimGray
+        Me.Label2.Location = New System.Drawing.Point(257, 3)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 14)
+        Me.Label2.TabIndex = 45
+        Me.Label2.Text = "Width"
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Meiryo UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.DimGray
+        Me.Label3.Location = New System.Drawing.Point(302, 3)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 14)
+        Me.Label3.TabIndex = 46
+        Me.Label3.Text = "Height"
+        '
+        'Label4
+        '
+        Me.Label4.Font = New System.Drawing.Font("Meiryo UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.DimGray
+        Me.Label4.Location = New System.Drawing.Point(347, 3)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 14)
+        Me.Label4.TabIndex = 47
+        Me.Label4.Text = "Left"
+        '
+        'Label5
+        '
+        Me.Label5.Font = New System.Drawing.Font("Meiryo UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.DimGray
+        Me.Label5.Location = New System.Drawing.Point(392, 3)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(39, 14)
+        Me.Label5.TabIndex = 48
+        Me.Label5.Text = "Top"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.Color.Black
+        Me.Label6.Font = New System.Drawing.Font("Meiryo UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.LightGray
+        Me.Label6.Location = New System.Drawing.Point(12, 13)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(14, 14)
+        Me.Label6.TabIndex = 49
+        Me.Label6.Text = "0"
+        '
+        'Version
+        '
+        Me.Version.AutoSize = True
+        Me.Version.Font = New System.Drawing.Font("Meiryo UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Version.ForeColor = System.Drawing.Color.DimGray
+        Me.Version.Location = New System.Drawing.Point(1136, 14)
+        Me.Version.Name = "Version"
+        Me.Version.Size = New System.Drawing.Size(59, 14)
+        Me.Version.TabIndex = 50
+        Me.Version.Text = "ver. 1.0.0"
+        '
+        'nW
+        '
+        Me.nW.Font = New System.Drawing.Font("Meiryo UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.nW.ForeColor = System.Drawing.Color.DimGray
+        Me.nW.Location = New System.Drawing.Point(257, 18)
+        Me.nW.Name = "nW"
+        Me.nW.Size = New System.Drawing.Size(39, 14)
+        Me.nW.TabIndex = 51
+        Me.nW.Text = "960"
+        Me.nW.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'nH
+        '
+        Me.nH.Font = New System.Drawing.Font("Meiryo UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.nH.ForeColor = System.Drawing.Color.DimGray
+        Me.nH.Location = New System.Drawing.Point(302, 18)
+        Me.nH.Name = "nH"
+        Me.nH.Size = New System.Drawing.Size(39, 14)
+        Me.nH.TabIndex = 52
+        Me.nH.Text = "540"
+        Me.nH.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'nL
+        '
+        Me.nL.Font = New System.Drawing.Font("Meiryo UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.nL.ForeColor = System.Drawing.Color.DimGray
+        Me.nL.Location = New System.Drawing.Point(347, 18)
+        Me.nL.Name = "nL"
+        Me.nL.Size = New System.Drawing.Size(39, 14)
+        Me.nL.TabIndex = 53
+        Me.nL.Text = "0"
+        Me.nL.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'nT
+        '
+        Me.nT.Font = New System.Drawing.Font("Meiryo UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.nT.ForeColor = System.Drawing.Color.DimGray
+        Me.nT.Location = New System.Drawing.Point(392, 18)
+        Me.nT.Name = "nT"
+        Me.nT.Size = New System.Drawing.Size(39, 14)
+        Me.nT.TabIndex = 54
+        Me.nT.Text = "1080"
+        Me.nT.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.DimGray
+        Me.Panel1.Location = New System.Drawing.Point(494, 6)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(10, 10)
+        Me.Panel1.TabIndex = 55
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.DimGray
+        Me.Panel2.Location = New System.Drawing.Point(510, 6)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(10, 10)
+        Me.Panel2.TabIndex = 56
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.DimGray
+        Me.Panel3.Location = New System.Drawing.Point(494, 22)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(10, 10)
+        Me.Panel3.TabIndex = 57
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.Color.DimGray
+        Me.Panel4.Location = New System.Drawing.Point(510, 22)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(10, 10)
+        Me.Panel4.TabIndex = 58
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.ClientSize = New System.Drawing.Size(1280, 760)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.nT)
+        Me.Controls.Add(Me.nL)
+        Me.Controls.Add(Me.nH)
+        Me.Controls.Add(Me.nW)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Version)
+        Me.Controls.Add(Me.maxbox)
+        Me.Controls.Add(Me.Closeing)
+        Me.Font = New System.Drawing.Font("Meiryo UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.ForeColor = System.Drawing.Color.White
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "CaptureX"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Task As Timer
+    Friend WithEvents Closeing As Button
+    Friend WithEvents maxbox As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Version As Label
+    Friend WithEvents nW As Label
+    Friend WithEvents nH As Label
+    Friend WithEvents nL As Label
+    Friend WithEvents nT As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel4 As Panel
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,208 @@
+﻿Option Strict On
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+    Dim FPS As Integer = 0
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Task.Enabled = True
+        Timer1.Enabled = True
+        'Form2.Show()
+    End Sub
+
+    Private Sub Task_Tick(sender As Object, e As EventArgs) Handles Task.Tick
+        If Timer1.Enabled = True Then
+            FPS += 1
+        End If
+
+        Dim hWidth = Integer.Parse(nW.Text)
+        Dim hHeight = Integer.Parse(nH.Text)
+        Dim hTop = Integer.Parse(nT.Text)
+        Dim hLeft = Integer.Parse(nL.Text)
+
+        If PictureBox1.Image IsNot Nothing Then
+            PictureBox1.Image.Dispose()
+        End If
+        Dim disDC As IntPtr = GetDC(IntPtr.Zero)
+        'Bitmapの作成
+        Dim bmp As New Bitmap(hWidth, hHeight)
+        'Graphicsの作成
+        Dim g As Graphics = Graphics.FromImage(bmp)
+        'Graphicsのデバイスコンテキストを取得
+        Dim hDC As IntPtr = g.GetHdc()
+        'Bitmapに画像をコピーする
+        BitBlt(hDC, 0, 0, bmp.Width, bmp.Height, disDC, hLeft, hTop, SRCCOPY)
+        '解放
+        g.ReleaseHdc(hDC)
+        g.Dispose()
+        ReleaseDC(IntPtr.Zero, disDC)
+        PictureBox1.Image = bmp
+    End Sub
+
+    Const SRCCOPY As Integer = 13369376
+
+    <DllImport("user32.dll")>
+    Private Shared Function GetDC(ByVal hwnd As IntPtr) As IntPtr
+    End Function
+
+    <DllImport("gdi32.dll")>
+    Private Shared Function BitBlt(ByVal hDestDC As IntPtr,
+        ByVal x As Integer, ByVal y As Integer,
+        ByVal nWidth As Integer, ByVal nHeight As Integer,
+        ByVal hSrcDC As IntPtr,
+        ByVal xSrc As Integer, ByVal ySrc As Integer,
+        ByVal dwRop As Integer) As Integer
+    End Function
+
+    <DllImport("user32.dll")>
+    Private Shared Function ReleaseDC(ByVal hwnd As IntPtr, ByVal hdc As IntPtr) As IntPtr
+
+    End Function
+
+
+    Public Shared Function CaptureScreen() As Bitmap
+        'プライマリモニタのデバイスコンテキストを取得
+        Dim disDC As IntPtr = GetDC(IntPtr.Zero)
+        'Bitmapの作成
+        Dim bmp As New Bitmap(960, 540)
+        'Graphicsの作成
+        Dim g As Graphics = Graphics.FromImage(bmp)
+        'Graphicsのデバイスコンテキストを取得
+        Dim hDC As IntPtr = g.GetHdc()
+        'Bitmapに画像をコピーする
+        BitBlt(hDC, 0, 0, bmp.Width, bmp.Height, disDC, 0, 1080, SRCCOPY)
+        '解放
+        g.ReleaseHdc(hDC)
+        g.Dispose()
+        ReleaseDC(IntPtr.Zero, disDC)
+        Return bmp
+    End Function
+
+
+    Private Sub Form1_Close(sender As Object, e As EventArgs) Handles MyBase.Closed
+
+    End Sub
+
+
+
+    Private mousePoint As Point
+
+    Private Sub Form1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseDown, PictureBox1.MouseDown
+        If (e.Button And MouseButtons.Left) = MouseButtons.Left Then
+            '位置を記憶する
+            mousePoint = New Point(e.X, e.Y)
+        End If
+    End Sub
+
+    'マウスが動いたとき
+    Private Sub Form1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseMove, PictureBox1.MouseMove
+        If (e.Button And MouseButtons.Left) = MouseButtons.Left Then
+            Me.Left += e.X - mousePoint.X
+            Me.Top += e.Y - mousePoint.Y
+        End If
+    End Sub
+
+    Dim mode As Integer = 0
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.DoubleClick, maxbox.Click
+        If mode = 2 Then
+            Me.WindowState = FormWindowState.Normal
+            Me.Width = 1280
+            Me.Height = 760
+            PictureBox1.Width = 1280
+            PictureBox1.Height = 720
+            PictureBox1.Top = 40
+            Closeing.Left = 1248
+            maxbox.Left = 1216
+            Version.Left = 1136
+            Try
+                AppActivate("Parsec")
+            Catch ex As Exception
+            End Try
+            mode = 0
+        ElseIf mode = 1 Then
+            Me.WindowState = FormWindowState.Maximized
+            PictureBox1.Width = Me.Width
+            PictureBox1.Height = Me.Height
+            PictureBox1.Top = 0
+            Try
+                AppActivate("Parsec")
+            Catch ex As Exception
+            End Try
+            mode = 2
+        ElseIf mode = 0 Then
+            Me.WindowState = FormWindowState.Normal
+            Me.Width = 1598
+            Me.Height = 938
+            PictureBox1.Width = 1600
+            PictureBox1.Height = 900
+            PictureBox1.Top = 40
+            Closeing.Left = 1568
+            maxbox.Left = 1536
+            Version.Left = 1456
+            Try
+                AppActivate("Parsec")
+            Catch ex As Exception
+            End Try
+            mode = 1
+        End If
+
+        '    If Me.WindowState = FormWindowState.Maximized Then
+        '    'Me.FormBorderStyle = FormBorderStyle.None
+        '    Me.WindowState = FormWindowState.Normal
+        '    Me.Width = 1280
+        '    Me.Height = 760
+        '    PictureBox1.Width = 1280
+        '    PictureBox1.Height = 720
+        '    PictureBox1.Top = 40
+        '    Me.TopMost = True
+        '    Try
+        '        AppActivate("Parsec")
+        '    Catch ex As Exception
+
+        '    End Try
+        'Else
+        '    Me.WindowState = FormWindowState.Maximized
+        '    PictureBox1.Width = Me.Width
+        '    PictureBox1.Height = Me.Height
+        '    PictureBox1.Top = 0
+        '    Me.TopMost = True
+        '    Try
+        '        AppActivate("Parsec")
+        '    Catch ex As Exception
+
+        '    End Try
+        'End If
+    End Sub
+
+    Private Sub Close_Click(sender As Object, e As EventArgs) Handles Closeing.Click
+        Me.Close()
+    End Sub
+
+    Private Sub PictureBox1_Click_1(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Try
+            AppActivate("Parsec")
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    'Private Sub TextBox_KeyPress(sender As Object, e As System.Windows.Forms.KeyPressEventArgs) Handles nW.KeyPress, nH.KeyPress, nL.KeyPress, nT.KeyPress
+    '    If (e.KeyChar < "0"c OrElse "9"c < e.KeyChar) AndAlso e.KeyChar <> ControlChars.Back Then
+    '        e.Handled = True
+    '    End If
+    'End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label6.Text = CStr(FPS)
+        FPS = 0
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+        If Timer1.Enabled = True Then
+            Timer1.Enabled = False
+            Label6.Text = "0"
+        Else
+            Timer1.Enabled = True
+        End If
+    End Sub
+End Class
